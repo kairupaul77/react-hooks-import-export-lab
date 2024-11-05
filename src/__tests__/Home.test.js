4c2ff9b8acae32fdabc236ea4d2618f011ec0e7f
paul kairu <paulkairukamau@gmail.com>
2024-11-05 21:01:14 +0300

    react-hooks-import-export-lab

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Home from "../components/Home";
+import Home from "../components/Home";  // Make sure this import is correct
 
 test("it is exported as a default export", () => {
   try {
+    // This checks if the component renders without errors
     expect(() => render(<Home />)).not.toThrow();
   } catch (e) {
+    // If rendering throws an error, this will catch and re-throw it with a custom message
     throw new Error("Make sure to export this component!");
   }
 });
--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,3 +1,5 @@
+// src/__tests__/user.test.js
+
 import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
